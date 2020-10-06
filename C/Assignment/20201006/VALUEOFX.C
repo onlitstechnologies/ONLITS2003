1838891a8d6612dc0d4a24ecacf7ffbd9b44d1c8
shiva kashyap <shivashahi32116@gmail.com>
2020-10-06 11:45:03 +0530

    Create VALUEOFX.C

---
+++ C/Assignment/20201006/VALUEOFX.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,x;
+clrscr();
+printf("Enter Values of a,b&c\n");
+scanf("%d%d%d",&a,&b,&c);
+x=a/(b-c);
+printf("result=%d",x);
+getch()
+}
