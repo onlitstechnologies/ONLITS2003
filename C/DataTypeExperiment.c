a7e2655228f0f5eeca862e4b5cd0b1085648c4dd
onlitstechnologies <onlitstechnologies@gmail.com>
2020-10-08 14:14:29 +0530

    ONLITS_20201008

---
+++ C/DataTypeExperiment.c
@@ -0,0 +1,14 @@
+//Data type experiment
+ 
+#include<stdio.h>
+
+int main()
+{
+	unsigned int a=65;
+	
+	printf("a=%c\n", a);
+	
+	printf("The size of a is %ld byte(s).\n", sizeof(a));
+	
+	return 0;
+}
