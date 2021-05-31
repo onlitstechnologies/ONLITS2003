aaff2cfdd152d879faaa8984dfb1e1d2e95cf616
onlitstechnologies <onlitstechnologies@gmail.com>
2021-05-31 14:58:32 +0530

    20210531

--- CPP/ComplexNumber.cpp
+++ CPP/ComplexNumber.cpp
@@ -1,28 +1,64 @@
 #include<iostream>
 using namespace std;
 
-class complex
+class complex                       //User defined data type
 {
     float real;
     float imag;
 public:
-    void display(complex p);
-    complex(float x, float y);      //constructor
+    complex();                      //default constructor
+    complex(float x, float y);      //parameterized constructor
+    void input(float x, float y);
+    void display();
+    complex add(complex p);
 };
 
-complex :: complex(float x, float y)
+complex :: complex()
+{
+    real = 0.0F;
+    imag = 0.0F;
+}
+
+complex :: complex(float x, float y)     //parameterized constructor
 {
     real = x;
     imag = y;
 }
 
-void complex :: display(complex p)
+void complex :: input(float x, float y)
 {
-    cout<<"real: "<<p.real<<endl;
-    cout<<"imag: "<<p.imag<<endl;
+    this->real = x;
+    imag = y;
+}
+
+void complex :: display()
+{
+    cout<<"real: "<<this->real<<endl;
+    cout<<"imag: "<<this->imag<<endl;
+}
+
+complex complex :: add(complex p)
+{
+    complex t;
+    t.real = real + p.real;
+    t.imag = imag + p.imag;
+    return t;
 }
 
 int main()
 {
-    complex c(10.3F, 20.6F);
+    complex c1(10.3F, 20.6F);
+    complex c2(25.6F,99.7F); 
+    complex c3;
+    c3 = c1.add(c2);
+    //c3 = c1 + c2;           //possible via operator overloading
+    c1.display();           //this is c1
+    c2.display();           //this is c2
+    c3.display();           //this has the address of c3
+    return 0;
 }
+
+/*
+    Class is the recipe -   
+    Object it the dish  -   Samosa
+*/
