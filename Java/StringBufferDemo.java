85b3b34e906e4dc8c684eba962e08880f8241bc5
onlitstechnologies <onlitstechnologies@gmail.com>
2021-07-05 17:59:03 +0530

    20210705

---
+++ Java/StringBufferDemo.java
@@ -0,0 +1,8 @@
+public class StringBufferDemo {
+    public static void main(String[] args) {
+        StringBuffer name = new StringBuffer("Shiva");
+        name.append(" ");
+        name.append("Kashyap");
+        System.out.println(name);
+    }
+}
